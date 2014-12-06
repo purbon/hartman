c5899a99c59be68fa52aed30628f3808576f947b
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-12-07 00:21:08 +0100

    make the define me command more clear

--- lib/handlers/hartman.rb
+++ lib/handlers/hartman.rb
@@ -4,17 +4,15 @@ module Lita
   module Handlers
     class Hartman < Handler
 
-      route(/^urbandict\s+(.+)/, :define, :help => { "Hartman: urbandict WORD" => "list of definitions found within the Urban Dictionary" })
+      route(/^define\s+me\s+(.+)/, :define, :help => { "Hartman: urbandict WORD" => "list of definitions found within the Urban Dictionary" })
 
       def define(response)
-        entries = ::Urban::Dictionary.new.lookup(response.args)
-        response.reply "Matches: " if entries
+        entries = ::Urban::Dictionary.new.lookup(response.matches)
+        response.reply_privately "Matches: " if entries
         count = 0
         entries.each do |entry|
-          response.reply "ID: #{count} #{entry[:def]}"
-          response.reply "Example: "
-          response.reply entry[:example]
-          response.reply "----"
+          msg = "ID: #{count} #{entry[:def]}\nExample: #{entry[:example]}"
+          response.reply_privately msg
           count+=1
         end
       end
