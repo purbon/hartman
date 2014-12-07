6738ddc12e6bd01cafa7646f42781c955412c3be
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-12-07 02:49:18 +0100

    add a ping command to hartman

--- lib/handlers/hartman.rb
+++ lib/handlers/hartman.rb
@@ -10,6 +10,11 @@ module Lita
 
       route(/^define\s+me\s+(.+)/, :defineme, :help => { "Hartman: define me {WORD}" => "list of definitions found within the Urban Dictionary" })
       route(/^weather\s+in\s+(.+)/, :weatherin, :help => { "Hartman: weather in {PLACE}" => "check the weather in a given place" })
+      route(/^ping/, :ping, :help => { "Hartman: ping" => "checking Hartman health" })
+
+      def ping(response)
+        response.reply "Dear @#{response.message.source.user.name rescue 'Dear'}, what can I do for you?"
+      end
 
     end
     Lita.register_handler(Hartman)
--- lib/hartman/weather/router.rb
+++ lib/hartman/weather/router.rb
@@ -5,7 +5,7 @@ module WeatherRouter
   def weatherin(response)
     data = Engine.new.check(response.matches)
     msg  = "at #{data['date']} is #{data['text']}, with #{((data['temp'].to_i-30)/2)}°C"
-    response.reply "Dear #{response.message.source.user.name}, weather #{msg}"
+    response.reply "Dear @#{response.message.source.user.name}, weather #{msg}"
   rescue
     puts "error retrieving data from yahoo weather"
   end
